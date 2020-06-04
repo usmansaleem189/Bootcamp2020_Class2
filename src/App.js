cc9924f8ad3adcda8a29942ba51e45a51bd5591b
Usman <m.u.saleem@student.tudelft.nl>
2020-06-04 16:58:04 +0500

    Adding local folder to git repo

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          <br/>
+          Updated by MU Saleem on 4th June
         </p>
         <a
           className="App-link"
